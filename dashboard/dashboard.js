d3e705a7ed50fefbf55ba0d0bcf984c403a6eee8
mayainun <mayainun45@gmail.com>
2024-05-10 11:00:39 +0700

    +ERD

--- dashboard/dashboard.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var logoutButton = document.getElementById('logoutButton');
-    logoutButton.addEventListener('click', function() {
-        window.location.href = 'index.html';
-    });
-});
